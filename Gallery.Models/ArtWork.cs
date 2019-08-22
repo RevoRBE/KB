e3d777dbfdfbaeba2d83bdc25d8e5b975fa47ba9
Krum Savov <krum.savov@gmail.com>
2019-08-22 16:20:23 +0300

    small changes in artwork model

--- Gallery.Models/ArtWork.cs
+++ Gallery.Models/ArtWork.cs
@@ -9,11 +9,12 @@ namespace Gallery.Models
         public int Id { get; set; }
 
         public int TypeId { get; set; }
+        public int SecondaryTypeId { get; set; }
         // public ArtWorkType Type { get; set; }
 
         public int SizeX { get; set; }
         public int SizeY { get; set; }
-        public int? Size { get; set; }
+        public int? SizeZ { get; set; }
 
         public ICollection<Material> Material { get; set; }
 
